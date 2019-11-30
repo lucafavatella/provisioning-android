1c7f4ba97e61d05d9e7d9fef6c087d1eeef5ea28
Luca Favatella <lucafavatella@users.noreply.github.com>
2019-11-30 02:46:41 +0000

    wip

--- lib/revoke-special-app-access.py
+++ lib/revoke-special-app-access.py
@@ -1,2 +1,9 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
-aa
+device = MonkeyRunner.waitForConnection()
+package = 'com.android.settings'
+activity = '.Settings$AppAndNotificationDashboardActivity'
+runComponent = package + '/' + activity
+device.startActivity(component=runComponent)
+result = device.takeSnapshot()
+result.writeToFile('shot1.png','png')
+
