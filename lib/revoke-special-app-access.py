f0dbbef19da205ce46c4c99005fb3ee6b5bd4343
Luca Favatella <lucafavatella@users.noreply.github.com>
2019-12-08 15:27:04 +0100

    XXX delete monkeyrunner

--- lib/revoke-special-app-access.py
+++
@@ -1,9 +0,0 @@
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
-device = MonkeyRunner.waitForConnection()
-package = 'com.android.settings'
-activity = '.Settings$AppAndNotificationDashboardActivity'
-runComponent = package + '/' + activity
-device.startActivity(component=runComponent)
-result = device.takeSnapshot()
-result.writeToFile('shot1.png','png')
-
