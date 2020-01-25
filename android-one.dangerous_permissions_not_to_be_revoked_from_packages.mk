911484d76aaa6e4296962ce940cee6e99578b65e
Luca Favatella <lucafavatella@users.noreply.github.com>
2020-01-25 18:13:37 +0100

    Keep call log permissions for phone app

--- android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
+++ android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
@@ -1,4 +1,6 @@
 dangerous_permissions_not_to_be_revoked_from_packages = \
 	android.permission.CALL_PHONE-from-com.google.android.dialer \
+	android.permission.READ_CALL_LOG-from-com.google.android.dialer \
 	android.permission.READ_PHONE_STATE-from-com.google.android.dialer \
+	android.permission.WRITE_CALL_LOG-from-com.google.android.dialer \
 	
