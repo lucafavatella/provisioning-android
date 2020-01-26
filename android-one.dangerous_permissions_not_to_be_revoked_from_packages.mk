0384d10cdc41e389b9d1a09879eac1fd333a7afe
Luca Favatella <lucafavatella@users.noreply.github.com>
2020-01-27 00:20:24 +0100

    Keep SMS app working

--- android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
+++ android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
@@ -3,4 +3,10 @@ dangerous_permissions_not_to_be_revoked_from_packages = \
 	android.permission.READ_CALL_LOG-from-com.google.android.dialer \
 	android.permission.READ_PHONE_STATE-from-com.google.android.dialer \
 	android.permission.WRITE_CALL_LOG-from-com.google.android.dialer \
+	android.permission.READ_CONTACTS-from-org.smssecure.smssecure \
+	android.permission.READ_PHONE_STATE-from-org.smssecure.smssecure \
+	android.permission.RECEIVE_MMS-from-org.smssecure.smssecure \
+	android.permission.RECEIVE_SMS-from-org.smssecure.smssecure \
+	android.permission.SEND_SMS-from-org.smssecure.smssecure \
+	android.permission.WRITE_CONTACTS-from-org.smssecure.smssecure \
 	
