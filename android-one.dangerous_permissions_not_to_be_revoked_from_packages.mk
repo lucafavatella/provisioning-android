7d372aa1777d9d27e8180e6e3dc564a95eac5f42
Luca Favatella <lucafavatella@users.noreply.github.com>
2020-02-27 22:48:11 +0100

    Review permissions not to be revoked based on running device

--- android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
+++ android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
@@ -7,9 +7,11 @@ dangerous_permissions_not_to_be_revoked_from_packages = \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-com.simplemobiletools.filemanager.pro \
 	android.permission.READ_EXTERNAL_STORAGE-from-com.simplemobiletools.gallery.pro \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-com.simplemobiletools.gallery.pro \
+	android.permission.CAMERA-from-com.whatsapp \
 	android.permission.READ_EXTERNAL_STORAGE-from-com.whatsapp \
 	android.permission.RECORD_AUDIO-from-com.whatsapp \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-com.whatsapp \
+	android.permission.ACCESS_FINE_LOCATION-from-net.osmand.plus \
 	android.permission.CAMERA-from-net.sourceforge.opencamera \
 	android.permission.READ_EXTERNAL_STORAGE-from-net.sourceforge.opencamera \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-net.sourceforge.opencamera \
