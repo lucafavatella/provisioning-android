4b8900887532791b07f82a7f4f2c02d7be6d3de7
Luca Favatella <lucafavatella@users.noreply.github.com>
2020-03-03 00:21:44 +0000

    Add some of the permissions detected on phone

--- android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
+++ android-one.dangerous_permissions_not_to_be_revoked_from_packages.mk
@@ -3,6 +3,8 @@ dangerous_permissions_not_to_be_revoked_from_packages = \
 	android.permission.READ_CALL_LOG-from-com.google.android.dialer \
 	android.permission.READ_PHONE_STATE-from-com.google.android.dialer \
 	android.permission.WRITE_CALL_LOG-from-com.google.android.dialer \
+	android.permission.READ_CONTACTS-from-com.simplemobiletools.contacts.pro \
+	android.permission.WRITE_CONTACTS-from-com.simplemobiletools.contacts.pro \
 	android.permission.READ_EXTERNAL_STORAGE-from-com.simplemobiletools.filemanager.pro \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-com.simplemobiletools.filemanager.pro \
 	android.permission.READ_EXTERNAL_STORAGE-from-com.simplemobiletools.gallery.pro \
