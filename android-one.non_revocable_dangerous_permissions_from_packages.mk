da06d648fc23693629abb2a33ffb3c979c93cd25
Luca Favatella <lucafavatella@users.noreply.github.com>
2020-05-25 16:23:29 +0100

    Revert "Refresh list of non-revocable permissions - following Android 10"
    This reverts commit e17e4800bc506730a17d66a3ce28c854a2a68dee.

--- android-one.non_revocable_dangerous_permissions_from_packages.mk
+++ android-one.non_revocable_dangerous_permissions_from_packages.mk
@@ -2,21 +2,40 @@ non_revocable_dangerous_permissions_from_packages = \
 	android.permission.GET_ACCOUNTS-from-android \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.bluetooth \
 	android.permission.GET_ACCOUNTS-from-com.android.bluetooth \
+	android.permission.READ_CALL_LOG-from-com.android.bluetooth \
 	android.permission.READ_CONTACTS-from-com.android.bluetooth \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.bluetooth \
+	android.permission.READ_SMS-from-com.android.bluetooth \
+	android.permission.RECEIVE_SMS-from-com.android.bluetooth \
+	android.permission.SEND_SMS-from-com.android.bluetooth \
+	android.permission.WRITE_CALL_LOG-from-com.android.bluetooth \
 	android.permission.WRITE_CONTACTS-from-com.android.bluetooth \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.bluetooth \
+	android.permission.RECEIVE_SMS-from-com.android.cellbroadcastreceiver \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.companiondevicemanager \
-	android.permission.ACCESS_FINE_LOCATION-from-com.android.companiondevicemanager \
 	android.permission.CALL_PHONE-from-com.android.emergency \
 	android.permission.READ_CONTACTS-from-com.android.emergency \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.externalstorage \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.externalstorage \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.location.fused \
 	android.permission.ACCESS_FINE_LOCATION-from-com.android.location.fused \
+	android.permission.READ_SMS-from-com.android.mms.service \
 	android.permission.READ_CONTACTS-from-com.android.nfc \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.nfc \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.nfc \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.phone \
 	android.permission.CALL_PHONE-from-com.android.phone \
 	android.permission.GET_ACCOUNTS-from-com.android.phone \
+	android.permission.PROCESS_OUTGOING_CALLS-from-com.android.phone \
+	android.permission.READ_CALL_LOG-from-com.android.phone \
 	android.permission.READ_CONTACTS-from-com.android.phone \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.phone \
+	android.permission.READ_SMS-from-com.android.phone \
+	android.permission.RECEIVE_SMS-from-com.android.phone \
 	android.permission.RECORD_AUDIO-from-com.android.phone \
+	android.permission.SEND_SMS-from-com.android.phone \
 	android.permission.USE_SIP-from-com.android.phone \
+	android.permission.WRITE_CALL_LOG-from-com.android.phone \
 	android.permission.WRITE_CONTACTS-from-com.android.phone \
 	com.android.voicemail.permission.ADD_VOICEMAIL-from-com.android.phone \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.printspooler \
@@ -27,32 +46,59 @@ non_revocable_dangerous_permissions_from_packages = \
 	android.permission.READ_CONTACTS-from-com.android.providers.contacts \
 	android.permission.READ_PHONE_STATE-from-com.android.providers.contacts \
 	android.permission.WRITE_CONTACTS-from-com.android.providers.contacts \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.providers.downloads \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.providers.downloads \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.providers.media \
+	android.permission.READ_PHONE_STATE-from-com.android.providers.media \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.providers.media \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.providers.telephony \
+	android.permission.RECEIVE_SMS-from-com.android.providers.telephony \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.providers.telephony \
+	android.permission.READ_CALL_LOG-from-com.android.server.telecom \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.server.telecom \
+	android.permission.WRITE_CALL_LOG-from-com.android.server.telecom \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.settings \
-	android.permission.ACCESS_FINE_LOCATION-from-com.android.settings \
 	android.permission.CALL_PHONE-from-com.android.settings \
-	android.permission.CAMERA-from-com.android.settings \
 	android.permission.READ_CONTACTS-from-com.android.settings \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.settings \
 	android.permission.READ_PHONE_STATE-from-com.android.settings \
+	android.permission.READ_SMS-from-com.android.settings \
+	android.permission.RECEIVE_SMS-from-com.android.settings \
+	android.permission.SEND_SMS-from-com.android.settings \
 	android.permission.WRITE_CONTACTS-from-com.android.settings \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.settings \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.sharedstoragebackup \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.sharedstoragebackup \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.shell \
 	android.permission.ACCESS_FINE_LOCATION-from-com.android.shell \
 	android.permission.CALL_PHONE-from-com.android.shell \
 	android.permission.GET_ACCOUNTS-from-com.android.shell \
 	android.permission.READ_CALENDAR-from-com.android.shell \
 	android.permission.READ_CONTACTS-from-com.android.shell \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.shell \
 	android.permission.READ_PHONE_STATE-from-com.android.shell \
+	android.permission.SEND_SMS-from-com.android.shell \
 	android.permission.WRITE_CALENDAR-from-com.android.shell \
 	android.permission.WRITE_CONTACTS-from-com.android.shell \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.shell \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.storagemanager \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.storagemanager \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.android.systemui \
 	android.permission.CAMERA-from-com.android.systemui \
 	android.permission.GET_ACCOUNTS-from-com.android.systemui \
 	android.permission.READ_CONTACTS-from-com.android.systemui \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.systemui \
 	android.permission.READ_PHONE_STATE-from-com.android.systemui \
 	android.permission.RECORD_AUDIO-from-com.android.systemui \
-	android.permission.ACCESS_MEDIA_LOCATION-from-com.android.vending \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.systemui \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.android.vending \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.android.vending \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.google.android.gms \
 	android.permission.ACCESS_FINE_LOCATION-from-com.google.android.gms \
-	android.permission.ACTIVITY_RECOGNITION-from-com.google.android.gms \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.google.android.packageinstaller \
+	android.permission.READ_EXTERNAL_STORAGE-from-org.codeaurora.bluetooth \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-org.codeaurora.bluetooth \
 	android.permission.CAMERA-from-org.codeaurora.ims \
+	android.permission.READ_EXTERNAL_STORAGE-from-org.codeaurora.ims \
 	android.permission.READ_PHONE_STATE-from-org.codeaurora.ims \
 	
--- sprout.extra_non_revocable_dangerous_permissions_from_packages.mk
+++ sprout.extra_non_revocable_dangerous_permissions_from_packages.mk
@@ -2,13 +2,17 @@ extra_non_revocable_dangerous_permissions_from_packages = \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.hmdglobal.app.activation \
 	android.permission.ACCESS_FINE_LOCATION-from-com.hmdglobal.app.activation \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.activation \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.hmdglobal.app.fmradio \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.fmradio \
 	android.permission.RECORD_AUDIO-from-com.hmdglobal.app.fmradio \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.hmdglobal.app.fmradio \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.omacp \
+	android.permission.RECEIVE_WAP_PUSH-from-com.hmdglobal.app.omacp \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.sarauth \
 	android.permission.GET_ACCOUNTS-from-com.hmdglobal.app.setupwizardext \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.setupwizardext \
-	android.permission.ACCESS_MEDIA_LOCATION-from-com.wos.face.service \
 	android.permission.CAMERA-from-com.wos.face.service \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.wos.face.service \
 	android.permission.RECORD_AUDIO-from-com.wos.face.service \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.wos.face.service \
 	
