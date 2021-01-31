7ed680901c0db67e320f8ce62b9a5363694004a9
Luca Favatella <lucafavatella@users.noreply.github.com>
2021-01-31 22:19:20 +0100

    Update non-revocable permissions following recent update

--- sprout.extra_non_revocable_dangerous_permissions_from_packages.mk
+++ sprout.extra_non_revocable_dangerous_permissions_from_packages.mk
@@ -2,10 +2,24 @@ extra_non_revocable_dangerous_permissions_from_packages = \
 	android.permission.ACCESS_COARSE_LOCATION-from-com.hmdglobal.app.activation \
 	android.permission.ACCESS_FINE_LOCATION-from-com.hmdglobal.app.activation \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.activation \
+	android.permission.ACCESS_MEDIA_LOCATION-from-com.hmdglobal.app.cdfinger \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.hmdglobal.app.cdfinger \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.hmdglobal.app.cdfinger \
+	android.permission.ACCESS_MEDIA_LOCATION-from-com.hmdglobal.app.customizationclient \
+	android.permission.PROCESS_OUTGOING_CALLS-from-com.hmdglobal.app.customizationclient \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.hmdglobal.app.customizationclient \
+	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.customizationclient \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.hmdglobal.app.customizationclient \
 	android.permission.READ_EXTERNAL_STORAGE-from-com.hmdglobal.app.fmradio \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.fmradio \
 	android.permission.RECORD_AUDIO-from-com.hmdglobal.app.fmradio \
 	android.permission.WRITE_EXTERNAL_STORAGE-from-com.hmdglobal.app.fmradio \
+	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.midfm \
+	android.permission.ACCESS_COARSE_LOCATION-from-com.hmdglobal.app.midtest \
+	android.permission.ACCESS_FINE_LOCATION-from-com.hmdglobal.app.midtest \
+	android.permission.READ_EXTERNAL_STORAGE-from-com.hmdglobal.app.midtest \
+	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.midtest \
+	android.permission.WRITE_EXTERNAL_STORAGE-from-com.hmdglobal.app.midtest \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.omacp \
 	android.permission.RECEIVE_WAP_PUSH-from-com.hmdglobal.app.omacp \
 	android.permission.READ_PHONE_STATE-from-com.hmdglobal.app.sarauth \
