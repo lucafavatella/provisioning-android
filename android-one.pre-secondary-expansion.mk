a8b3ce52f6a999f18e965d1551dd63349b95f36c
Luca Favatella <lucafavatella@users.noreply.github.com>
2022-02-06 12:01:31 +0000

    Enable unclear com.android.networkstack.tethering.inprocess - popped up in 11

--- android-one.pre-secondary-expansion.mk
+++ android-one.pre-secondary-expansion.mk
@@ -76,6 +76,7 @@ google_packages_not_to_be_disabled = \
 	com.android.location.fused \
 	com.android.networkstack.inprocess \
 	com.android.networkstack.permissionconfig \
+	com.android.networkstack.tethering.inprocess \
 	com.android.nfc \
 	com.android.ons \
 	com.android.printspooler \
