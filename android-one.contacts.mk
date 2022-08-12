68418373246c07bf0478c897165898089e4dd971
Luca Favatella <lucafavatella@users.noreply.github.com>
2022-08-12 20:04:30 +0100

    Plant command to dump contacts

--- android-one.contacts.mk
+++ android-one.contacts.mk
@@ -4,3 +4,8 @@ ADB = adb
 
 .PHONY: list-devices
 list-devices: ; $(ADB) devices -l
+
+# Reference: https://android.stackexchange.com/questions/176172/exporting-contacts-via-adb/241758#241758
+.PHONY: dump-contacts
+dump-contacts:
+	$(ADB) shell content query --uri content://com.android.contacts/data
